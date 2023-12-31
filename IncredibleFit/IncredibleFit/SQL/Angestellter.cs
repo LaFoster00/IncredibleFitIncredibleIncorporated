82106606a834c9ed0e8e04a326b482a67988e931
Lasse Foster <lasse.foster@web.de>
2023-12-31 16:44:01 +0100

    * marked nullable database entries as nulable and added specific mappings

--- IncredibleFit/IncredibleFit/SQL/Angestellter.cs
+++ IncredibleFit/IncredibleFit/SQL/Angestellter.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
 
 namespace IncredibleFit.IncredibleFit.SQL
 {
     [Entity("ANGESTELLTER")]
     public class Angestellter
     {
-        [Field("ID")] public int Id { get; private set; } = -1;
+        [Field("ID", OracleDbType.Int32), ServersideSetup] public int Id { get; private set; } = 25;
 
-        [Field("NAME")] public string Name { get; set; } = String.Empty;
+        [Field("NAME", OracleDbType.Varchar2)] public string Name { get; set; }
 
-        [Field("VORNAME")] public string? Vorname { get; set; } = null;
+        [Field("VORNAME", OracleDbType.Varchar2)] public string? Vorname { get; set; }
 
-        [Field("GEHALT")] public double? Gehalt { get; set; } = null;
+        [Field("GEHALT", OracleDbType.Double)] public double? Gehalt { get; set; }
 
-        [Field("KOMISSION")] public double? Komission { get; set; } = null;
+        [Field("KOMMISSION")] public double? Kommission { get; set; }
 
-        [Field("CHEF")] public int? Chef { get; set; } = null;
+        [Field("CHEF")] public int? Chef { get; set; }
+
+        private Angestellter() : this(String.Empty) { }
+
+        public Angestellter(string name, string? vorname = null, double? gehalt = null, double? kommission = null,
+            int? chef = null)
+        {
+            Name = name;
+            Vorname = vorname;
+            Gehalt = gehalt;
+            Kommission = kommission;
+            Chef = chef;
+        }
     }
 }
