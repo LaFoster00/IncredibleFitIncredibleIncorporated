d1cf35cafb38da2e6d63664e2bcda7b7038e4298
Lisa Weickenmeier <lisaweickenmeier@outlook.de>
2023-12-31 11:36:10 +0100

    Empty Screenfiles and DataType Exercise.cs

---
+++ IncredibleFit/IncredibleFit/Models/Exercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncredibleFit.IncredibleFit.Models
+{
+    //[Entity("EXERCISE")]
+    public class Exercise
+    {
+        //[Field("id")]
+        public int id { get; private set; }
+        //[Field("name")]
+        public string name { get; set; }
+        //[Field("description")]
+        public string description { get; set; }
+        //[Field("duration")]
+        public double duration { get; set; }
+
+        public Exercise(int id, string name, string description, double duration) 
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.duration = duration;
+        }
+    }
+}
