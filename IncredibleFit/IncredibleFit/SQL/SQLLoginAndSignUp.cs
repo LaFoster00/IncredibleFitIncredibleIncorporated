de3bef82b7669d5afbbc02adbb76d61db3f53fc1
Lasse Foster <lasse.foster@web.de>
2024-01-11 19:39:04 +0100

    * renamed sqllogin

---
+++ IncredibleFit/IncredibleFit/SQL/SQLLoginAndSignUp.cs
@@ -0,0 +1,58 @@
+﻿using IncredibleFit.SQL;
+using IncredibleFit.SQL.Entities;
+
+namespace IncredibleFit.IncredibleFit.SQL
+{
+    public class AccountTakenException : Exception
+    {
+        public AccountTakenException() {}
+        public AccountTakenException(string message) : base(message) {}
+        public AccountTakenException(string message, Exception inner) : base(message, inner) {}
+    }
+
+    public class UserInvalidException : Exception
+    {
+        public UserInvalidException() { }
+        public UserInvalidException(string message) : base(message) { }
+        public UserInvalidException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public static class SQLLoginAndSignUp
+    {
+        public static User? GetUserWithEmail(in string email)
+        {
+            var reader = OracleDatabase.ExecuteQuery(OracleDatabase.CreateCommand(
+                $"""
+                 SELECT * FROM "USER"
+                 WHERE EMAIl = '{email}'
+                 """));
+
+            var users = reader.ToObjectList<User>();
+            return users.Any() ? users[0] : null;
+        }
+
+        public static void UpdatePassword(User user, in string password)
+        {
+            if (string.IsNullOrEmpty(user.Salt))
+            {
+                throw new UserInvalidException("User doesn't have salt and therefore doesn't exist.");
+            }
+
+            user.Password = PasswordUtil.Hash(password, user.Salt);
+            OracleDatabase.UpdateObject(user);
+        }
+
+        public static User CreateNewUser(string email, string firstName, string name)
+        {
+            User? user = SQLLoginAndSignUp.GetUserWithEmail(email);
+            if (user != null)
+            {
+                throw new AccountTakenException();
+            }
+
+            user = new User(email, firstName, name);
+            OracleDatabase.InsertObject(user);
+            return user;
+        }
+    }
+}
