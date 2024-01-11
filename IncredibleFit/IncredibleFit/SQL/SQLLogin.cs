c412557fc0921f566d61063d01f9c8a775e10155
Lasse Foster <lasse.foster@web.de>
2024-01-11 17:58:40 +0100

    * prompt wrong credentials

---
+++ IncredibleFit/IncredibleFit/SQL/SQLLogin.cs
@@ -0,0 +1,20 @@
+﻿using IncredibleFit.SQL;
+using IncredibleFit.SQL.Entities;
+
+namespace IncredibleFit.IncredibleFit.SQL
+{
+    public static class SQLLogin
+    {
+        public static User? GetUserWithEmail(in string email)
+        {
+            var reader = OracleDatabase.ExecuteQuery(OracleDatabase.CreateCommand(
+                $"""
+                 SELECT * FROM "USER"
+                 WHERE EMAIl = '{email}'
+                 """));
+
+            var users = reader.ToObjectList<User>();
+            return users.Any() ? users[0] : null;
+        }
+    }
+}
