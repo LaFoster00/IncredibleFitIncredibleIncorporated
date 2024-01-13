05eb390033d7c1b129b64dd7a73ec078ecbb0aa2
Lasse Foster <lasse.foster@web.de>
2024-01-13 19:43:24 +0100

    *ingredient

--- IncredibleFit/IncredibleFit/SQL/Entities/Ingredient.cs
+++ IncredibleFit/IncredibleFit/SQL/Entities/Ingredient.cs
@@ -3,25 +3,88 @@
 namespace IncredibleFit.SQL.Entities
 {
     [Entity("INGREDIENT")]
-    public class Ingredient
+    public class Ingredient : BindableObject
     {
-        [ID, Field("INGREDIENTNAME", OracleDbType.Varchar2 ,64)]
-        public string IngredientName { get; set; } = String.Empty;
+
+        [ID, Field("INGREDIENTNAME", OracleDbType.Varchar2, 64)]
+        public string IngredientName
+        {
+            get => (string)GetValue(IngredientNameProperty);
+            set => SetValue(IngredientNameProperty, value);
+        }
 
         [Field("FOODCATEGORY", OracleDbType.Int16)] //Domain in ERD
-        public FoodCategory Foodcategory { get; set; } = FoodCategory.Invalid;
+        public FoodCategory Foodcategory
+        {
+            get => (FoodCategory)GetValue(FoodcategoryProperty);
+            set => SetValue(FoodcategoryProperty, value);
+        }
 
         [Field("CALORIES", OracleDbType.Int16)]
-        public Int16 Calories { get; set; } = -1;
+        public short Calories
+        {
+            get => (short)GetValue(CaloriesProperty);
+            set => SetValue(CaloriesProperty, value);
+        }
 
         [Field("PROTEIN", OracleDbType.Int16)]
-        public Int16? Protein { get; set; } = 0;
+        public short? Protein
+        {
+            get => (short?)GetValue(ProteinProperty);
+            set => SetValue(ProteinProperty, value);
+        }
 
         [Field("FAT", OracleDbType.Int16)]
-        public Int16? Fat { get; set; } = 0;
+        public short? Fat
+        {
+            get => (short?)GetValue(FatProperty);
+            set => SetValue(FatProperty, value);
+        }
 
         [Field("CARBONHYDRATES", OracleDbType.Int16)]
-        public Int16? Carbonhydrates { get; set; } = 0;
+        public short? Carbonhydrates
+        {
+            get => (short?)GetValue(CarbonhydratesProperty);
+            set => SetValue(CarbonhydratesProperty, value);
+        }
+
+        public static readonly BindableProperty IngredientNameProperty =
+            BindableProperty.Create(
+                nameof(IngredientName),
+                typeof(string),
+                typeof(Ingredient),
+                string.Empty);
+
+        public static readonly BindableProperty FoodcategoryProperty =
+            BindableProperty.Create(
+                nameof(Foodcategory), 
+                typeof(FoodCategory), 
+                typeof(Ingredient), 
+                FoodCategory.Invalid);
+
+        public static readonly BindableProperty CaloriesProperty =
+            BindableProperty.Create(
+                nameof(Calories), 
+                typeof(short), 
+                typeof(Ingredient), 
+                (short)-1);
+
+        public static readonly BindableProperty ProteinProperty =
+            BindableProperty.Create(
+                nameof(Protein), 
+                typeof(short?), 
+                typeof(Ingredient),
+                null);
+
+        public static readonly BindableProperty FatProperty =
+            BindableProperty.Create(
+                nameof(Fat), 
+                typeof(short?), 
+                typeof(Ingredient), 
+                null);
+
+        public static readonly BindableProperty CarbonhydratesProperty =
+            BindableProperty.Create(nameof(Carbonhydrates), typeof(short?), typeof(Ingredient), (short?)0);
 
         private Ingredient() { }
 
