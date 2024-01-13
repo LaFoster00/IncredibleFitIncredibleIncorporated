7064be9706dd84e4b11d298f9fff7559e65ef0f8
Lasse Foster <lasse.foster@web.de>
2024-01-13 20:09:15 +0100

    * Recipecategory

--- IncredibleFit/IncredibleFit/SQL/Entities/Recipecategory.cs
+++ IncredibleFit/IncredibleFit/SQL/Entities/Recipecategory.cs
@@ -3,16 +3,49 @@
 namespace IncredibleFit.SQL.Entities
 {
     [Entity("RECIPECATEGORY")]
-    public class Recipecategory
+    public class Recipecategory : BindableObject
     {
-        [ID, Field("RECIPECATEGORYID", OracleDbType.Int32)]
-        public int RecipeCategoryID { get; private set; } = -1;
+        [ID, Field("RECIPECATEGORYID", OracleDbType.Int32), AutoIncrement]
+        public int RecipeCategoryID
+        {
+            get => (int)GetValue(RecipeCategoryIDProperty);
+            private set => SetValue(RecipeCategoryIDProperty, value);
+        }
 
         [Field("MEALTYPE", OracleDbType.Int16)] //Domain in ERD
-        public MealType Mealtype { get; set; } = MealType.Invalid;
+        public MealType Mealtype
+        {
+            get => (MealType)GetValue(MealtypeProperty);
+            set => SetValue(MealtypeProperty, value);
+        }
 
         [Field("FOODCATEGORY", OracleDbType.Int16)] //Domain in ERD
-        public FoodCategory Foodcategory { get; set;} = MealType.Invalid;
+        public FoodCategory Foodcategory
+        {
+            get => (FoodCategory)GetValue(FoodcategoryProperty);
+            set => SetValue(FoodcategoryProperty, value);
+        }
+
+        public static readonly BindableProperty RecipeCategoryIDProperty =
+            BindableProperty.Create(
+                nameof(RecipeCategoryID), 
+                typeof(int), 
+                typeof(Recipecategory), 
+                -1);
+
+        public static readonly BindableProperty MealtypeProperty =
+            BindableProperty.Create(
+                nameof(Mealtype), 
+                typeof(MealType), 
+                typeof(Recipecategory), 
+                MealType.Invalid);
+
+        public static readonly BindableProperty FoodcategoryProperty =
+            BindableProperty.Create(
+                nameof(Foodcategory), 
+                typeof(FoodCategory), 
+                typeof(Recipecategory), 
+                FoodCategory.Invalid);
 
         private Recipecategory() { }
         public Recipecategory(short mealtype, short foodcategory)
