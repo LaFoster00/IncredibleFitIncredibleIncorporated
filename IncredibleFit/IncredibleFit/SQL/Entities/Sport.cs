a0ca4f897985b61c3ec0a143d8e167b0320ef077
Lasse Foster <lasse.foster@web.de>
2024-01-14 11:44:56 +0100

    * sport

--- IncredibleFit/IncredibleFit/SQL/Entities/Sport.cs
+++ IncredibleFit/IncredibleFit/SQL/Entities/Sport.cs
@@ -3,16 +3,49 @@
 namespace IncredibleFit.SQL.Entities
 {
     [Entity("SPORT")]
-    internal class Sport
+    internal class Sport : BindableObject
     {
-        [ID, Field("SPORTID", OracleDbType.Int32)]
-        public int SportID { get; private set; } = -1;
+        [ID, Field("SPORTID", OracleDbType.Int32), AutoIncrement]
+        public int SportID
+        {
+            get => (int)GetValue(SportIDProperty);
+            private set => SetValue(SportIDProperty, value);
+        }
 
         [Field("NAME", OracleDbType.Varchar2, 128)]
-        public string Name { get; set; } = string.Empty;
+        public string Name
+        {
+            get => (string)GetValue(NameProperty);
+            set => SetValue(NameProperty, value);
+        }
 
         [Field("DESCRIPTION", OracleDbType.Varchar2, 1024)]
-        public string Description { get; set; } = string.Empty;
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public static readonly BindableProperty SportIDProperty =
+            BindableProperty.Create(
+                nameof(SportID), 
+                typeof(int), 
+                typeof(Sport), 
+                -1);
+
+        public static readonly BindableProperty NameProperty =
+            BindableProperty.Create(
+                nameof(Name), 
+                typeof(string), 
+                typeof(Sport), 
+                string.Empty);
+
+        public static readonly BindableProperty DescriptionProperty =
+            BindableProperty.Create(
+                nameof(Description), 
+                typeof(string), 
+                typeof(Sport), 
+                string.Empty);
 
         private Sport() { }
 
