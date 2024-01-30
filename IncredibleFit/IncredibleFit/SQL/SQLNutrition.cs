64ee06c04c48abbe0264aee5ea4cf068dfd85c5e
Lisa Weickenmeier <lisaweickenmeier@outlook.de>
2024-01-30 14:05:36 +0100

    Documentation in SQLTraining, SQLTimeline and SQLNutrition

--- IncredibleFit/IncredibleFit/SQL/SQLNutrition.cs
+++ IncredibleFit/IncredibleFit/SQL/SQLNutrition.cs
@@ -1,6 +1,7 @@
 ﻿// Written by Lisa Weickenmeier https://github.com/LisaWckn
 
 using IncredibleFit.SQL.Entities;
+using MetalPerformanceShaders;
 using Oracle.ManagedDataAccess.Client;
 using System.Collections.ObjectModel;
 
@@ -8,6 +9,12 @@ namespace IncredibleFit.SQL
 {
     public static class SQLNutrition
     {
+        /// <summary>
+        /// Retrieves a collection of recipes based on visibility criteria for a specific user from the database.
+        /// The visibility of the recipe must either be public or it must be created by the user.
+        /// </summary>
+        /// <param name="user">The user for whom recipes are to be retrieved.</param>
+        /// <returns>An ObservableCollection of Recipe objects based on visibility criteria.</returns>
         public static ObservableCollection<Recipe> getAllVisibleRecipes(User user)
         {
             ObservableCollection<Recipe> recipes = new ObservableCollection<Recipe>();
@@ -21,23 +28,19 @@ namespace IncredibleFit.SQL
             var reader = OracleDatabase.ExecuteQuery(command);
 
             var track = reader.ToObjectList<Recipe>();
-            if (track.Any())
-            {
-                foreach ( var item in track)
-                {
-                    Recipecategory? recipeCat = SQLNutrition.getRecipeCategory(item);
-                    if (recipeCat != null)
-                    {
-                        item.MealType = recipeCat.Mealtype;
-                        item.FoodCategory = recipeCat.Foodcategory;
-                    }
-                    recipes.Add(item);
-                }
-            }
 
-            return recipes;
+            return recipesWithCategorys(track);
         }
 
+        /// <summary>
+        /// Retrieves a collection of recipes based on keyword and ingredient criteria for a specific user from the database.
+        /// </summary>
+        /// <param name="keyword">The keyword to match against recipe names.</param>
+        /// <param name="ingredientname">The ingredient name to filter recipes.</param>
+        /// <param name="user">The user for whom recipes are to be retrieved.</param>
+        /// <returns>
+        /// An ObservableCollection of Recipe objects based on keyword and ingredient criteria.
+        /// </returns>
         public static ObservableCollection<Recipe> getRecipesByIngredientAndKeyword(string keyword, string ingredientname, User user)
         {
             ObservableCollection<Recipe> recipes = new ObservableCollection<Recipe>();
@@ -46,44 +49,40 @@ namespace IncredibleFit.SQL
 
             if (ingredientname == "")
             {
+                // If no ingredient name is provided, query for recipes based on the keyword and user email.
                 command = OracleDatabase.CreateCommand(
                     $"""
-                 SELECT * FROM "RECIPE"
-                 WHERE (VISIBILITY=1 OR (VISIBILITY = 0 AND EMAIL = '{user.Email}')) AND NAME LIKE '%{keyword}%' 
-                 """);
+                     SELECT * FROM "RECIPE"
+                     WHERE (VISIBILITY=1 OR (VISIBILITY = 0 AND EMAIL = '{user.Email}')) AND NAME LIKE '%{keyword}%' 
+                     """);
             }
             else
             {
+                // If an ingredient name is provided, join with the "RECIPEINGREDIENT" table and filter by both keyword and ingredient.
                 command = OracleDatabase.CreateCommand(
-                                    $"""
-                 SELECT * FROM "RECIPE"
-                 JOIN "RECIPEINGREDIENT"
-                 ON RECIPEINGREDIENT.RECIPEID = RECIPE.RECIPEID
-                 WHERE (RECIPE.VISIBILITY=1 OR (RECIPE.VISIBILITY = 0 AND RECIPE.EMAIL = '{user.Email}')) 
-                 AND RECIPE.NAME LIKE '%{keyword}%' AND RECIPEINGREDIENT.INGREDIENTNAME = '{ingredientname}'
-                 """);
+                    $"""
+                     SELECT * FROM "RECIPE"
+                     JOIN "RECIPEINGREDIENT"
+                     ON RECIPEINGREDIENT.RECIPEID = RECIPE.RECIPEID
+                     WHERE (RECIPE.VISIBILITY=1 OR (RECIPE.VISIBILITY = 0 AND RECIPE.EMAIL = '{user.Email}')) 
+                     AND RECIPE.NAME LIKE '%{keyword}%' AND RECIPEINGREDIENT.INGREDIENTNAME = '{ingredientname}'
+                     """);
             }
 
             var reader = OracleDatabase.ExecuteQuery(command);
 
             var track = reader.ToObjectList<Recipe>();
-            if (track.Any())
-            {
-                foreach (var item in track)
-                {
-                    Recipecategory? recipeCat = SQLNutrition.getRecipeCategory(item);
-                    if (recipeCat != null)
-                    {
-                        item.MealType = recipeCat.Mealtype;
-                        item.FoodCategory = recipeCat.Foodcategory;
-                    }
-                    recipes.Add(item);
-                }
-            }
 
-            return recipes;
+            return recipesWithCategorys(track);
         }
 
+        /// <summary>
+        /// Retrieves a collection of favorite recipes for a specific user from the database.
+        /// </summary>
+        /// <param name="user">The user for whom favorite recipes are to be retrieved.</param>
+        /// <returns>
+        /// An ObservableCollection of Recipe objects representing the favorite recipes of the user.
+        /// </returns>
         public static ObservableCollection<Recipe> getFavoriteRecipes(User user)
         {
             ObservableCollection<Recipe> recipes = new ObservableCollection<Recipe>();
@@ -102,11 +101,31 @@ namespace IncredibleFit.SQL
             var reader = OracleDatabase.ExecuteQuery(command);
 
             var track = reader.ToObjectList<Recipe>();
+
+            return recipesWithCategorys(track);
+        }
+
+        /// <summary>
+        /// Associates recipe category information with a collection of recipes.
+        /// </summary>
+        /// <param name="track">The list of recipes to associate with category information.</param>
+        /// <returns>
+        /// An ObservableCollection of Recipe objects with updated MealType and FoodCategory properties based on their associated recipe category.
+        /// </returns>
+        public static ObservableCollection<Recipe> recipesWithCategorys(List<Recipe>? track)
+        {
+            ObservableCollection<Recipe> recipes = new ObservableCollection<Recipe>();
+
+            if(track == null)
+            {
+                return recipes;
+            }
+
             if (track.Any())
             {
                 foreach (var item in track)
                 {
-                    Recipecategory? recipeCat = SQLNutrition.getRecipeCategory(item);
+                    Recipecategory? recipeCat = getRecipeCategory(item);
                     if (recipeCat != null)
                     {
                         item.MealType = recipeCat.Mealtype;
@@ -119,31 +138,14 @@ namespace IncredibleFit.SQL
             return recipes;
         }
 
-        public static bool isRecipeFavorite(Recipe recipe, User user)
-        {
-            var command = OracleDatabase.CreateCommand(
-                $"""
-                 SELECT * FROM "USER_SAVED_RECIPES"
-                 WHERE EMAIL='{user.Email}' AND RECIPEID={recipe.RecipeID}
-                 """);
-
-            var reader = OracleDatabase.ExecuteQuery(command);
-
-            var track = reader.ToObjectList<UserSavedRecipe>();
-            return track.Any() ? true : false;
-        }
-        public static void addToFavorites(Recipe recipe, User user)
-        {
-            UserSavedRecipe usR = new UserSavedRecipe(recipe.RecipeID, user.Email);
-            OracleDatabase.InsertObject(usR);
-        }
-
-        public static void deleteFromFavorites(Recipe recipe, User user)
-        {
-            UserSavedRecipe usR = new UserSavedRecipe(recipe.RecipeID, user.Email);
-            OracleDatabase.DeleteObject(usR);
-        }
-
+        /// <summary>
+        /// Retrieves the recipe category information for a specific recipe.
+        /// </summary>
+        /// <param name="recipe">The recipe for which to retrieve the category information.</param>
+        /// <returns>
+        /// A Recipecategory object representing the category information for the specified recipe,
+        /// or null if no category information is found.
+        /// </returns>
         public static Recipecategory? getRecipeCategory(Recipe recipe)
         {
             var command = OracleDatabase.CreateCommand(
@@ -159,6 +161,57 @@ namespace IncredibleFit.SQL
             return track.Any() ? track[0] : null;
         }
 
+        /// <summary>
+        /// Checks if a recipe is marked as a favorite by a specific user.
+        /// </summary>
+        /// <param name="recipe">The recipe to check for favorite status.</param>
+        /// <param name="user">The user for whom the favorite status is checked.</param>
+        /// <returns>
+        /// Returns true if the recipe is marked as a favorite by the user; otherwise, returns false.
+        /// </returns>
+        public static bool isRecipeFavorite(Recipe recipe, User user)
+        {
+            var command = OracleDatabase.CreateCommand(
+                $"""
+                 SELECT * FROM "USER_SAVED_RECIPES"
+                 WHERE EMAIL='{user.Email}' AND RECIPEID={recipe.RecipeID}
+                 """);
+
+            var reader = OracleDatabase.ExecuteQuery(command);
+
+            var track = reader.ToObjectList<UserSavedRecipe>();
+            return track.Any() ? true : false;
+        }
+
+        /// <summary>
+        /// Adds a recipe to the list of favorites for a specific user.
+        /// </summary>
+        /// <param name="recipe">The recipe to be added to the favorites.</param>
+        /// <param name="user">The user for whom the recipe is added to favorites.</param>
+        public static void addToFavorites(Recipe recipe, User user)
+        {
+            UserSavedRecipe usR = new UserSavedRecipe(recipe.RecipeID, user.Email);
+            OracleDatabase.InsertObject(usR);
+        }
+
+        /// <summary>
+        /// Removes a recipe from the list of favorites for a specific user.
+        /// </summary>
+        /// <param name="recipe">The recipe to be removed from favorites.</param>
+        /// <param name="user">The user for whom the recipe is removed from favorites.</param>
+        public static void deleteFromFavorites(Recipe recipe, User user)
+        {
+            UserSavedRecipe usR = new UserSavedRecipe(recipe.RecipeID, user.Email);
+            OracleDatabase.DeleteObject(usR);
+        }
+
+        /// <summary>
+        /// Retrieves a collection of ingredients associated with a specific recipe.
+        /// </summary>
+        /// <param name="recipe">The recipe for which to retrieve the associated ingredients.</param>
+        /// <returns>
+        /// An ObservableCollection of Ingredient objects representing the ingredients associated with the specified recipe.
+        /// </returns>
         public static ObservableCollection<Ingredient> getIngredientsByRecipe(Recipe recipe)
         {
             ObservableCollection<Ingredient> ingredients = new ObservableCollection<Ingredient>();
@@ -177,7 +230,7 @@ namespace IncredibleFit.SQL
 
             var track = reader.ToObjectList<Ingredient>();
 
-            for(int i=0; i < track.Count; i++)
+            for (int i = 0; i < track.Count; i++)
             {
                 ingredients.Add(track[i]);
             }
@@ -185,6 +238,14 @@ namespace IncredibleFit.SQL
             return ingredients;
         }
 
+        /// <summary>
+        /// Retrieves the quantity of a specific ingredient used in a given recipe.
+        /// </summary>
+        /// <param name="recipe">The recipe in which to check the quantity of the ingredient.</param>
+        /// <param name="ingredient">The ingredient for which to retrieve the quantity.</param>
+        /// <returns>
+        /// The quantity of the specified ingredient used in the given recipe, or 0 if the ingredient is not found.
+        /// </returns>
         public static decimal getQuantityOfIngredient(Recipe recipe, Ingredient ingredient)
         {
             var command = OracleDatabase.CreateCommand(
@@ -200,6 +261,14 @@ namespace IncredibleFit.SQL
             return track.Any() ? track[0].Quantity : 0;
         }
 
+        /// <summary>
+        /// Retrieves the quantity unit associated with a specific ingredient.
+        /// </summary>
+        /// <param name="ingredient">The ingredient for which to retrieve the quantity unit.</param>
+        /// <returns>
+        /// The QuantityUnit enum representing the quantity unit associated with the specified ingredient,
+        /// or QuantityUnit.Invalid if the ingredient is not found.
+        /// </returns>
         public static QuantityUnit getQuantityUnitOfIngredient(Ingredient ingredient)
         {
             var command = OracleDatabase.CreateCommand(
@@ -215,28 +284,36 @@ namespace IncredibleFit.SQL
             return track.Any() ? track[0].Quantityunit : QuantityUnit.Invalid;
         }
 
-        public static Track? getTrackByDate(DateTime date, User? user)
+        /// <summary>
+        /// Retrieves the user's track data for a specific date.
+        /// </summary>
+        /// <param name="date">The date for which to retrieve track data.</param>
+        /// <param name="user">The user for whom track data is to be retrieved.</param>
+        /// <returns>
+        /// A Track object representing the user's track data for the specified date,
+        /// or null if no track data is found for the given date and user.
+        /// </returns>
+        public static Track? getTrackByDate(DateTime date, User user)
         {
-            string mail = "";
-            if (user != null)
-            {
-                mail = user.Email;
-            }
             DateTime date2 = new DateTime(date.Year, date.Month, date.Day);
-            
+
             var command = OracleDatabase.CreateCommand(
                 $"""
                  SELECT * FROM "TRACK"
-                 WHERE "DATE" = :PDATE AND EMAIL='{mail}'
+                 WHERE "DATE" = :PDATE AND EMAIL='{user.Email}'
                  """);
             command.Parameters.Add(new OracleParameter("PDATE", OracleDbType.Date)).Value = date2;
-            
-            var reader = OracleDatabase.ExecuteQuery(command);
 
+            var reader = OracleDatabase.ExecuteQuery(command);
             var track = reader.ToObjectList<Track>();
+
             return track.Any() ? track[0] : null;
         }
 
+        /// <summary>
+        /// Saves or updates a Calorie Track in the database for a specific date and user.
+        /// </summary>
+        /// <param name="calorieTrack">The Calorie Track object to be saved or updated.</param>
         public static void SaveCalorieTrack(Track calorieTrack)
         {
             DateTime date2 = new DateTime(calorieTrack.Date.Year, calorieTrack.Date.Month, calorieTrack.Date.Day);
@@ -251,8 +328,8 @@ namespace IncredibleFit.SQL
             var reader = OracleDatabase.ExecuteQuery(command);
 
             var track = reader.ToObjectList<Track>();
-            
-            if(!track.Any()) 
+
+            if (!track.Any())
             {
                 OracleDatabase.InsertObject(calorieTrack);
             }
--- IncredibleFit/IncredibleFit/SQL/SQLTraining.cs
+++ IncredibleFit/IncredibleFit/SQL/SQLTraining.cs
@@ -7,6 +7,10 @@ namespace IncredibleFit.SQL
 {
     public static class SQLTraining
     {
+        /// <summary>
+        /// Retrieves all training plans from the database.
+        /// </summary>
+        /// <returns>An ObservableCollection containing all TrainingPlan objects.</returns>
         public static ObservableCollection<TrainingPlan> getAllTrainingPlans()
         {
             ObservableCollection<TrainingPlan> TrainingPlans = new ObservableCollection<TrainingPlan>();
@@ -30,10 +34,17 @@ namespace IncredibleFit.SQL
 
             return TrainingPlans;
         }
+
+        /// <summary>
+        /// Retrieves the current training plan associated with a specific user from the database.
+        /// </summary>
+        /// <param name="user">The user for whom the current training plan is to be retrieved.</param>
+        /// <returns>
+        /// If a current training plan is found for the specified user, returns the associated TrainingPlan object;
+        /// otherwise, returns null.
+        /// </returns>
         public static TrainingPlan? getCurrentTrainingPlan(User user)
         {
-            //Get selected plan by user
-
             var command = OracleDatabase.CreateCommand(
                 $"""
                  SELECT *
@@ -52,6 +63,10 @@ namespace IncredibleFit.SQL
             return track.Any() ? track[0] : null;
         }
 
+        /// <summary>
+        /// Deletes the current training plan associated with a specific user and the associated appointments from the database.
+        /// </summary>
+        /// <param name="user">The user for whom the current training plan is to be deleted.</param>
         public static void deleteCurrentTrainingplan(User user)
         {
             var command = OracleDatabase.CreateCommand(
@@ -80,6 +95,11 @@ namespace IncredibleFit.SQL
 
         }
 
+        /// <summary>
+        /// Sets a new training plan for a specific user, creating the necessary associations and appointments.
+        /// </summary>
+        /// <param name="trainingPlan">The new training plan to be set for the user.</param>
+        /// <param name="user">The user for whom the new training plan is to be set.</param>
         public static void setNewTrainingplan(TrainingPlan trainingPlan, User user)
         {
             UserPlan userPlan = new UserPlan(trainingPlan.TrainingPlanID, user.Email);
@@ -88,6 +108,14 @@ namespace IncredibleFit.SQL
             SQLTimeline.insertAppointmentsForNewTrainingPlan(trainingPlan, user);
         }
 
+        /// <summary>
+        /// Retrieves a list of plan training units associated with a specific training plan from the database.
+        /// </summary>
+        /// <param name="trainingPlan">The training plan for which plan training units are to be retrieved.</param>
+        /// <returns>
+        /// If the training plan is not null, returns a list of PlanTrainingUnit objects associated with the training plan;
+        /// otherwise, returns null.
+        /// </returns>
         public static List<PlanTrainingUnit>? getPlanTrainingUnitsByTrainingPlan(TrainingPlan trainingPlan)
         {
             if(trainingPlan == null)
@@ -108,6 +136,14 @@ namespace IncredibleFit.SQL
             return track;
         }
 
+        /// <summary>
+        /// Retrieves the training unit associated with a specific plan training unit from the database.
+        /// </summary>
+        /// <param name="unit">The plan training unit for which the associated training unit is to be retrieved.</param>
+        /// <returns>
+        /// If the plan training unit is not null, returns the associated TrainingUnit object;
+        /// otherwise, returns null.
+        /// </returns>
         public static TrainingUnit? getTrainingUnit(PlanTrainingUnit unit)
         {
             if(unit == null)
@@ -132,6 +168,11 @@ namespace IncredibleFit.SQL
             return track.Any() ? track[0] : null;
         }
 
+        /// <summary>
+        /// Retrieves the count of exercise units associated with a specific training unit from the database.
+        /// </summary>
+        /// <param name="trainingUnit">The training unit for which the count of associated exercise units is to be retrieved.</param>
+        /// <returns>The count of exercise units associated with the training unit. Returns 0 if the training unit is null.</returns>
         public static int getExerciseCount(TrainingUnit trainingUnit)
         {
             if(trainingUnit == null)
@@ -139,11 +180,19 @@ namespace IncredibleFit.SQL
                 return 0; 
             }
 
-            ObservableCollection<ExerciseUnit> exercises = SQLTraining.getExerciseUnits(trainingUnit);
+            ObservableCollection<ExerciseUnit> exercises = getExerciseUnits(trainingUnit);
 
             return exercises.Count();
         }
 
+        /// <summary>
+        /// Retrieves the next scheduled training unit for a specific user from the database.
+        /// </summary>
+        /// <param name="user">The user for whom the next scheduled training unit is to be retrieved.</param>
+        /// <returns>
+        /// If there is a next scheduled training unit, returns the associated TrainingUnit object;
+        /// otherwise, returns null.
+        /// </returns>
         public static TrainingUnit? getNextTrainingUnit(User user)
         {
             var command = OracleDatabase.CreateCommand(
@@ -178,15 +227,20 @@ namespace IncredibleFit.SQL
             return track2.Any() ? track2[0] : null;
         }
 
-        public static void setTrainingUnitDone(TrainingUnit? unit)
+        /// <summary>
+        /// Marks a specific training unit as done for a specific user by updating the status of associated appointments in the database.
+        /// </summary>
+        /// <param name="unit">The training unit to be marked as done.</param>
+        /// <param name="user">The user for whom the training unit is to be marked as done.</param>
+        public static void setTrainingUnitDone(TrainingUnit? unit, User user)
         {
             if(unit == null) { return; }
 
-            //Set Appointment with unit done in database
             var command = OracleDatabase.CreateCommand(
                 $"""
                  SELECT * FROM "APPOINTMENT" 
-                 WHERE TRAININGUNITID = {unit.TrainingUnitID}
+                 JOIN "USER_APPOINTMENT" ON APPOINTMENT.APPOINTMENTID = USER_APPOINTMENT.APPOINTMENTID
+                 WHERE APPOINTMENT.TRAININGUNITID = {unit.TrainingUnitID} AND USER_APPOINTMENT.EMAIL = '{user.Email}'
                  """);
 
             var reader = OracleDatabase.ExecuteQuery(command);
@@ -202,6 +256,14 @@ namespace IncredibleFit.SQL
             OracleDatabase.UpdateObject(appointment);
         }
 
+        /// <summary>
+        /// Retrieves a collection of exercise units associated with a specific training unit from the database.
+        /// </summary>
+        /// <param name="trainingUnit">The training unit for which exercise units are to be retrieved.</param>
+        /// <returns>
+        /// If the training unit is not null, returns an ObservableCollection of ExerciseUnit objects associated with the training unit;
+        /// otherwise, returns an empty ObservableCollection.
+        /// </returns>
         public static ObservableCollection<ExerciseUnit> getExerciseUnits(TrainingUnit trainingUnit)
         {
             if(trainingUnit == null)
@@ -235,6 +297,14 @@ namespace IncredibleFit.SQL
             return exerciseUnits;
         }
 
+        /// <summary>
+        /// Retrieves the exercise associated with a specific exercise unit from the database.
+        /// </summary>
+        /// <param name="exerciseUnit">The exercise unit for which the associated exercise is to be retrieved.</param>
+        /// <returns>
+        /// If there is an associated exercise, returns the associated Exercise object;
+        /// otherwise, returns null.
+        /// </returns>
         public static Exercise? getExerciseByExerciseUnit(ExerciseUnit exerciseUnit)
         {
             var command = OracleDatabase.CreateCommand(
